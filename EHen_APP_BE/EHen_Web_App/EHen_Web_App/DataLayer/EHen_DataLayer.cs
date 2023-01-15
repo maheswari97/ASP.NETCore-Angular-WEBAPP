aff247c04156760880b5e0e99440a58e88863481
maheswari97 <mahes.selvam97@gmail.com>
2023-01-15 17:44:10 +0100

    Add files via upload

---
+++ EHen_APP_BE/EHen_Web_App/EHen_Web_App/DataLayer/EHen_DataLayer.cs
@@ -0,0 +1,135 @@
+﻿using EHen_Web_App.Model;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EHen_Web_App.DataLayer
+{
+    public class EHen_DataLayer
+    {
+        String ConnectionString = "data source=LAPTOP-Q3E3C9NA;initial catalog=EHen_DB;trusted_connection=true";
+        public DataTable Pindex_DL()
+        {
+            DataTable dt = new DataTable();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConnectionString;
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Get_Price_index", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+
+            }
+
+            finally { con.Close(); }
+            return dt;
+        }
+
+
+
+        public int Edit_Pindex_DL(int ID,double Price)
+        {
+            int rowsAffected = 0;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConnectionString;
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Edit_price_index", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("@price", SqlDbType.VarChar).Value = Price;
+                cmd.Parameters.Add("@ID", SqlDbType.VarChar).Value = ID;
+
+                 rowsAffected = cmd.ExecuteNonQuery(); 
+            }
+
+            finally { con.Close(); }
+            return rowsAffected;
+        }
+
+
+        public DataSet Get_Hens_detail_DL(string Date=null)
+        {
+            DataSet ds = new DataSet();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConnectionString;
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Get_Hens_Detail", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                if(Date != null)
+                {
+                    cmd.Parameters.Add("@Date", SqlDbType.Date).Value = Date;
+                }
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+
+            }
+
+            finally { con.Close(); }
+
+            return ds;
+
+
+        }
+
+        public int Add_Hens_DL (int Hierarchy_level, int Hen_count,bool New_Hierarchy)
+        {
+            int rowsAffected = 0;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConnectionString;
+            string Stored_procedure_name = "";
+            try
+            {
+                con.Open();
+                if (New_Hierarchy == true)
+                    Stored_procedure_name = "Edit_Hens_table_with_New_Hierarchy";
+                else
+                    Stored_procedure_name = "Add_Hens";
+
+                SqlCommand cmd = new SqlCommand(Stored_procedure_name, con);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("@Hierarchy_level", SqlDbType.VarChar).Value = Hierarchy_level;
+                cmd.Parameters.Add("@Hen_count", SqlDbType.VarChar).Value = Hen_count;
+
+                rowsAffected = cmd.ExecuteNonQuery();
+            }
+
+            finally { con.Close(); }
+            return rowsAffected;
+        }
+
+        public int Edit_Hens_DL(int Hierarchy_level, int Hen_id,int Egg_count=0)
+        {
+            int rowsAffected = 0;
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConnectionString;
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Edit_hens", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("@Hierarchy_level", SqlDbType.VarChar).Value = Hierarchy_level;
+               
+                cmd.Parameters.Add("@Hen_ID", SqlDbType.VarChar).Value = Hen_id;
+
+                if(Egg_count!=0)
+                    cmd.Parameters.Add("@Egg_num", SqlDbType.VarChar).Value = Egg_count;
+
+                rowsAffected = cmd.ExecuteNonQuery();
+            }
+
+            finally { con.Close(); }
+            return rowsAffected;
+        }
+    }
+}
---
+++ EHen_APP_BE/EHen_Web_App/EHen_Web_App/Service/EHen_App_BusinessLayer.cs
@@ -0,0 +1,142 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using EHen_Web_App.Model;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using EHen_Web_App.DataLayer;
+
+namespace EHen_Web_App.Service
+{
+    public class EHen_App_BusinessLayer
+    {
+        EHen_DataLayer DL= new EHen_DataLayer();
+       
+
+
+        public DataTable Pindex()
+        {
+            DataTable dt= new DataTable();
+            dt = DL.Pindex_DL();
+            return dt;
+
+        }
+
+        public String edit_price_index(int ID, double price)
+        {
+            
+          int Rows_affected = DL.Edit_Pindex_DL(ID,price);
+          if(Rows_affected> 0)
+            {
+                return "Updated Successfully";
+            }
+            else
+            {
+                return "Update Failed! Network Error Please try again";
+            }
+
+        }
+
+
+        public List<Return_data> Get_Hens_detail(string Date=null)
+        {
+            DataSet ds=new DataSet();
+            DataTable HenDT= new DataTable();
+            DataTable HierarchyDT = new DataTable();
+            double basePrice = 0;
+            List<Return_data> list1 = new List<Return_data>();        
+            try
+            {
+                if(Date!=null)
+                ds = DL.Get_Hens_detail_DL(Date);
+                else
+                 ds = DL.Get_Hens_detail_DL();
+
+                HierarchyDT = ds.Tables[1];
+                HenDT = ds.Tables[2];
+                basePrice = Convert.ToDouble(ds.Tables[0].Rows[0][0]);
+                for(int i=0;i<HierarchyDT.Rows.Count;i++)
+                {
+                    int Egg_count_Temp = 0;
+                    int Egg_count = 0;
+                    int Hens_count = 0;
+                    double Per_EggPrice = 0;
+                    List<int> hens_Id = new List<int>();
+                    List<int> Eggs_num_perHen= new List<int>();
+
+
+                    Return_data Data1 = new Return_data();
+                    for (int j=0;j<HenDT.Rows.Count;j++)
+                    {
+                       
+                        if (Convert.ToInt16(HierarchyDT.Rows[i]["Hierarchy_level"]) == Convert.ToInt16(HenDT.Rows[j]["Hierarchy_level"])  + 1)
+                        {
+                            Egg_count_Temp = Egg_count_Temp + Convert.ToInt16(HenDT.Rows[j]["Egg_num"]);
+                        }
+                        if(Convert.ToInt16(HierarchyDT.Rows[i]["Hierarchy_level"])==1 && Convert.ToInt16(HenDT.Rows[j]["Hierarchy_level"])==1)
+                        {
+                            Egg_count_Temp = 0;
+                        }
+
+                        if (Convert.ToInt16(HierarchyDT.Rows[i]["Hierarchy_level"]) == Convert.ToInt16(HenDT.Rows[j]["Hierarchy_level"]))
+                        {
+                            Hens_count = Hens_count + 1;
+                            Egg_count = Convert.ToInt16(HenDT.Rows[j]["Egg_num"]) + Egg_count;
+                            hens_Id.Add(Convert.ToInt16(HenDT.Rows[j]["Hen_Id"]));
+                            Eggs_num_perHen.Add(Convert.ToInt16(HenDT.Rows[j]["Egg_num"]));
+                        }
+                    }
+                    Per_EggPrice = basePrice + Egg_count_Temp * basePrice * 0.01;
+
+                    Data1.Per_EggPrice = Per_EggPrice;
+                    Data1.Egg_count = Egg_count;
+                    Data1.Hen_count = Hens_count;
+                    Data1.Hierarchy_level =Convert.ToInt16( HierarchyDT.Rows[i]["Hierarchy_level"]);
+                    Data1.Hens_ID = hens_Id;
+                    Data1.Egg_Count_perHen = Eggs_num_perHen;
+
+                    list1.Add(Data1);
+                }
+
+            }
+
+            catch(Exception ex) { 
+            
+            }
+            return list1;
+        }
+
+
+        public String Add_Hens(int Hierarchy_level, int Hens_count,bool New_Hierarchy=false)
+        {
+
+            int Rows_affected = DL.Add_Hens_DL(Hierarchy_level, Hens_count, New_Hierarchy);
+            if (Rows_affected > 0)
+            {
+                return "Updated Successfully";
+            }
+            else
+            {
+                return "Update Failed! Network Error Please try again";
+            }
+
+        }
+
+        public String Edit_Hens(int Hierarchy_level, int Hen_ID, int Egg_count=0)
+        {
+
+            int Rows_affected = DL.Edit_Hens_DL(Hierarchy_level, Hen_ID, Egg_count);
+            if (Rows_affected > 0)
+            {
+                return "Updated Successfully";
+            }
+            else
+            {
+                if (Egg_count > 0)
+                    return "Cannot add Eggs to a New hens on the day they appear you can add only on following days ";
+                else
+                return "Update Failed! Network Error Please try again";
+            }
+
+        }
+
+    }
+}
